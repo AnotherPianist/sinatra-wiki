7fbb892930998b0b198037ba54f796418e963a9f
ThePianist <cafarimo@outlook.com>
2023-06-26 18:23:12 -0400

    Create new pages

--- main.rb
+++ main.rb
@@ -8,12 +8,27 @@ rescue Errno::ENOENT
   nil
 end
 
+def save_content(title, content)
+  File.open("pages/#{title}.txt", 'w') do |file|
+    file.puts(content)
+  end
+end
+
 get '/' do
   erb :welcome
+end
+
+get '/new' do
+  erb :new
 end
 
 get '/:title' do
   @title = params[:title]
   @content = page_content(@title)
   erb :show
+end
+
+post '/create' do
+  save_content(params['title'], params['content'])
+  redirect CGI.escape("/#{params['title']}")
 end
